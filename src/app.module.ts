85921c20a60408214e5d23fa4f2378b596602ddc
nellfs <pokycelit@gmail.com>
2023-01-28 14:53:21 -0300

    restart

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
-import { BookmarkModule } from "./bookmark/bookmark.module";
-import { PrismaModule } from "./prisma/prisma.module";
-import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule,
-    UserModule,
-    BookmarkModule,
-    PrismaModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
